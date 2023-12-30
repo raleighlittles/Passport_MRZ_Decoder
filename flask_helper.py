829dc9692311bc592e5423427bdeadf69bbcf899
Raleigh Littles III <raleighlittles@gmail.com>
2023-12-29 20:26:42 -0800

    flask now starts correctly

--- flask_helper.py
+++ flask_helper.py
@@ -1,9 +1,10 @@
 import string
 import socket
 import os
+import secrets
 
 def generate_secret_key(length):
-    
+
     password = ''.join(secrets.choice((string.ascii_letters + string.digits)) for i in range(length))
     return password
 
--- web_form.py
+++ web_form.py
@@ -1,13 +1,14 @@
 import flask_wtf
+import flask_wtf.file
 import wtforms
 
 from passport_mrz_td3_decoder import PASSPORT_MRZ_LINE_LENGTH
 
 class SubmitPassportMRZForm(flask_wtf.FlaskForm):
 
-    mrz_line_1 = wtforms.StringField("mrz_line_1", validators=[wtforms.validators.Length(min=PASSPORT_MRZ_LINE_LENGTH-1, max=PASSPORT_MRZ_LINE_LENGTH+1)])
-    mrz_line_2 = wtforms.StringField("mrz_line_2", validators=[wtforms.validators.Length(min=PASSPORT_MRZ_LINE_LENGTH-1, max=PASSPORT_MRZ_LINE_LENGTH+1)])
+    mrz_line_1 = wtforms.StringField(label="mrz_line_1", validators=[wtforms.validators.Length(min=PASSPORT_MRZ_LINE_LENGTH-1, max=PASSPORT_MRZ_LINE_LENGTH+1)])
+    mrz_line_2 = wtforms.StringField(label="mrz_line_2", validators=[wtforms.validators.Length(min=PASSPORT_MRZ_LINE_LENGTH-1, max=PASSPORT_MRZ_LINE_LENGTH+1)])
 
-    passport_img_file = flask_wtf.FileField()
+    passport_img_file = flask_wtf.file.FileField(label="Passport image")
 
     submit = wtforms.SubmitField("Submit")
