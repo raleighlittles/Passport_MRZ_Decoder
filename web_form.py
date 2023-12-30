c348396cac088c567c2e8ac4a46848129639394b
Raleigh Littles III <raleighlittles@gmail.com>
2023-12-29 21:06:58 -0800

    trying to clean up appearance of page

--- web_form.py
+++ web_form.py
@@ -6,8 +6,8 @@ from passport_mrz_td3_decoder import PASSPORT_MRZ_LINE_LENGTH
 
 class SubmitPassportMRZForm(flask_wtf.FlaskForm):
 
-    mrz_line_1 = wtforms.StringField(label="mrz_line_1", validators=[wtforms.validators.Length(min=PASSPORT_MRZ_LINE_LENGTH-1, max=PASSPORT_MRZ_LINE_LENGTH+1)])
-    mrz_line_2 = wtforms.StringField(label="mrz_line_2", validators=[wtforms.validators.Length(min=PASSPORT_MRZ_LINE_LENGTH-1, max=PASSPORT_MRZ_LINE_LENGTH+1)])
+    mrz_line_1 = wtforms.StringField(label="Line 1", validators=[wtforms.validators.Length(min=PASSPORT_MRZ_LINE_LENGTH-1, max=PASSPORT_MRZ_LINE_LENGTH+1)])
+    mrz_line_2 = wtforms.StringField(label="Line 2", validators=[wtforms.validators.Length(min=PASSPORT_MRZ_LINE_LENGTH-1, max=PASSPORT_MRZ_LINE_LENGTH+1)])
 
     passport_img_file = flask_wtf.file.FileField(label="Passport image")
 
